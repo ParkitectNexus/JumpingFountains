13f84124f6ee671fefe630c1716a59efbc63e5ef
Luuk Holleman <luukholleman@gmail.com>
2016-01-17 13:41:22 +0100

    Initial commit

---
+++ CustomScenery/JumpingFountain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Random = UnityEngine.Random;
+
+public class JumpingFountain : MonoBehaviour
+{
+    private List<ParticleSystem> _jets;
+
+    // Use this for initialization
+    void Start()
+    {
+        _jets = new List<ParticleSystem>(transform.GetComponentsInChildren<ParticleSystem>());
+
+        StartCoroutine(LoopJets());
+    }
+
+    private IEnumerator LoopJets()
+    {
+        for (;;)
+        {
+            ParticleSystem jet = _jets.OrderBy(x => Guid.NewGuid()).FirstOrDefault();
+
+            jet.Play();
+
+            yield return new WaitForSeconds(Random.value * 2);
+        }
+    }
+}
